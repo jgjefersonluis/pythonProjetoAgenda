d5af88e05be959b4a9890ca1c92c496210a97403
Jeferson Luis <jgjefersonluis@gmail.com>
2020-08-29 13:12:07 -0300

    erro 404

--- contatos/views.py
+++ contatos/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Contato
 
 
@@ -9,9 +10,21 @@ def index(request):
     })
 
 def ver_contato(request, contato_id):
-    contato = Contato.objects.get(id=contato_id)
+    contato = get_object_or_404(Contato, id=contato_id)
     return render(request, 'contatos/ver_contato.html', {
                   'contato': contato})
+
+"""
+def ver_contato(request, contato_id):
+    try:
+        contato = Contato.objects.get(id=contato_id)
+        return render(request, 'contatos/ver_contato.html', {
+                  'contato': contato})
+    except Contato.DoesNotExist as e:
+        raise Http404()
+"""
+
+
 
 
 
