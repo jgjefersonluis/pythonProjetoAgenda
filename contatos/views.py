b33c3b4e6c0e011fbb5554cd30180f145399dd2a
Jeferson Luis <jgjefersonluis@gmail.com>
2020-08-29 00:00:07 -0300

    exibindo um unico contato

--- contatos/views.py
+++ contatos/views.py
@@ -6,9 +6,13 @@ def index(request):
     contatos = Contato.objects.all()
     return render(request, 'contatos/index.html', {
         'contatos': contatos
-
-
     })
+
+def ver_contato(request, contato_id):
+    contato = Contato.objects.get(id=contato_id)
+    return render(request, 'contatos/ver_contato.html', {
+                  'contato': contato})
+
 
 
 
